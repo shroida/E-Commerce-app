58bb40d4a800231451b1cc85006e1f5f0b31f2d8
shroida <shroida012@gmail.com>
2025-01-29 10:07:30 +0200

    init path is splash screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,23 +1,23 @@
-import 'package:e_commerce_app/features/splash%20screen/ui/splash_screen.dart';
+import 'package:e_commerce_app/core/routing/routes.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static final router = GoRouter(
     routes: [
       GoRoute(
-        path: '/',
+        path: Routes.home,
         pageBuilder: (context, state) => HomePage(),
       ),
       GoRoute(
-        path: '/products',
+        path:Routes.loginScreen,
         pageBuilder: (context, state) => ProductsPage(),
       ),
       GoRoute(
-        path: '/cart',
+        path: Routes.registerScreen,
         pageBuilder: (context, state) => CartPage(),
       ),
       // Add more routes here as needed
     ],
-    initialLocation: SplashScreen(),
+    initialLocation:  Routes.splashScreen,
   );
 }
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,5 @@
 class Routes {
-  static const String home = '/';
+  static const String home = '/home';
   static const String splashScreen = '/splashScreen';
   static const String loginScreen = '/loginScreen';
   static const String registerScreen = '/registerScreen';
