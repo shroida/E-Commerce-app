59338c40b099c9b31c8ffd5620e0a8b7f50739a2
shroida <shroida012@gmail.com>
2025-01-29 11:00:59 +0200

    colors manager for app's color

---
+++ lib/core/theming/colors_manager.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color primaryColor = Color(0xFFEF5350); 
+  static const Color secondaryColor = Color(0xFF00BFA5);
+  static const Color accentColor = Color(0xFFFFC107);
+  static const Color backgroundColor = Color(0xFFF5F5F5); 
+  static const Color cardColor = Color(0xFFFFFFFF); 
+  static const Color textColorPrimary = Color(0xFF212121); 
+  static const Color textColorSecondary = Color(0xFF757575);
+  static const Color borderColor = Color(0xFFE0E0E0); 
+  static const Color saleBadgeColor = Color(0xFFFF7043);
+  static const Color successColor = Color(0xFF4CAF50); 
+}
