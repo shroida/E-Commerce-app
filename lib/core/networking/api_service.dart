771b6417d6cdd828e458ffd51895a6e1383fbdaa
shroida <shroida012@gmail.com>
2025-01-30 06:04:57 +0200

    create api service for signup

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:e_commerce_app/core/networking/api_constants.dart';
+import 'package:e_commerce_app/features/register/data/signup_request.dart';
+import 'package:e_commerce_app/features/register/data/signup_response.dart';
+import 'package:retrofit/http.dart';
+part 'api_service.g.dart';
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {required String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(
+    @Body() SignupRequest signupRequest,
+  );
+}
