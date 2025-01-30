5d8736df2581ef68c4a685c8b223a0177bc8b606
shroida <shroida012@gmail.com>
2025-01-30 05:10:38 +0200

    create signup request

---
+++ lib/features/register/data/signup_request.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'signup_request.g.dart';
+@JsonSerializable()
+class SignupRequest {
+  String name;
+  String email;
+  String password;
+  String phone;
+  SignupRequest({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.phone,
+  });
+}
