a59d4d9e49eabdc0b328db01c57df9dbb781e99d
shroida <shroida012@gmail.com>
2025-01-30 05:15:00 +0200

    generate signup request json

---
+++ lib/features/register/data/signup_request.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signup_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SignupRequest _$SignupRequestFromJson(Map<String, dynamic> json) =>
+    SignupRequest(
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+      phone: json['phone'] as String,
+    );
+
+Map<String, dynamic> _$SignupRequestToJson(SignupRequest instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+      'password': instance.password,
+      'phone': instance.phone,
+    };
