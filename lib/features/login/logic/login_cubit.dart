6469ae2077c15535fa8ae1f8eaa24cd7dd0a3b92
shroida <shroida012@gmail.com>
2025-01-30 17:12:07 +0200

    init login cubit file

---
+++ lib/features/login/logic/login_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:e_commerce_app/features/login/logic/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<LoginState>{
+    LoginCubit():super (LoginState.logininItial());
+
+}
