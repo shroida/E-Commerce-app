27be6fe24e2a75e04160ebc71415aefae063cad3
shroida <shroida012@gmail.com>
2025-01-30 06:35:42 +0200

    dio factory

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,7 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+Future<void> setUpGetIt()async{
+  Dio dio =DioFactory.get
+}
--- lib/features/register/logic/signup_cubit.dart
+++ lib/features/register/logic/signup_cubit.dart
@@ -1,18 +1,29 @@
-import 'package:e_commerce_app/features/register/data/signup_request.dart';
-import 'package:e_commerce_app/features/register/logic/signup_state.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:e_commerce_app/features/register/data/signup_request.dart';
+import 'package:e_commerce_app/features/register/logic/signup_state.dart';
+import 'package:e_commerce_app/features/register/repos/signup_repo.dart';
+
 class SignupCubit extends Cubit<SignupState> {
-  Sign
-  SignupCubit() : super(const SignupState.initial());
+  final SignupRepo _signupRepo;
+  SignupCubit(
+    this._signupRepo,
+  ) : super(const SignupState.initial());
 
   final formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
 
-  void emitSignupStates(){
-
+  void emitSignupStates() {
+    _signupRepo.signup(SignupRequest(
+        name: nameController.text,
+        email: emailController.text,
+        password: passwordController.text,
+        phone: phoneController.text));
   }
 }
