c21906fd129846017c82cb828d81cead3cb65b3e
shroida <shroida012@gmail.com>
2025-01-31 07:46:22 +0200

    create profile response

--- lib/features/profile/data/model/profile_request.g.dart
+++ lib/features/profile/data/model/profile_request.g.dart
@@ -6,12 +6,6 @@ part of 'profile_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-ProfileRequest _$ProfileRequestFromJson(Map<String, dynamic> json) =>
-    ProfileRequest(
-      token: json['token'] as String,
-      lang: json['lang'] as String,
-    );
-
 Map<String, dynamic> _$ProfileRequestToJson(ProfileRequest instance) =>
     <String, dynamic>{
       'token': instance.token,
---
+++ lib/features/profile/data/model/profile_response.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:json_annotation/json_annotation.dart';
+
+part 'profile_response.g.dart';
+
+@JsonSerializable()
+class ProfileResponse {
+  bool status;
+  String message;
+  @JsonKey(name: 'data')
+  UserData? userData;
+  ProfileResponse({
+    required this.status,
+    required this.message,
+    required this.userData,
+  });
+  factory ProfileResponse.fromJson(Map<String,dynamic> json) => _$ProfileResponseFromJson(json);
+}
+
+@JsonSerializable()
+class UserData {
+  int? id;
+  String name;
+  String email;
+  String phone;
+  String token;
+  UserData({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.token,
+  });
+  factory UserData.fromJson(Map<String,dynamic> json) => _$UserDataFromJson(json);
+}
