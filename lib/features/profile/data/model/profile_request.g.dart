e31fae281a048399de8313489ccd608f4228a212
shroida <shroida012@gmail.com>
2025-01-31 07:47:31 +0200

    generate respone profile json

--- lib/features/profile/data/model/profile_request.g.dart
+++ lib/features/profile/data/model/profile_request.g.dart
@@ -6,6 +6,12 @@ part of 'profile_request.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+ProfileRequest _$ProfileRequestFromJson(Map<String, dynamic> json) =>
+    ProfileRequest(
+      token: json['token'] as String,
+      lang: json['lang'] as String,
+    );
+
 Map<String, dynamic> _$ProfileRequestToJson(ProfileRequest instance) =>
     <String, dynamic>{
       'token': instance.token,
---
+++ lib/features/profile/data/model/profile_response.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ProfileResponse _$ProfileResponseFromJson(Map<String, dynamic> json) =>
+    ProfileResponse(
+      status: json['status'] as bool,
+      message: json['message'] as String,
+      userData: json['data'] == null
+          ? null
+          : UserData.fromJson(json['data'] as Map<String, dynamic>),
+    );
+
+
+UserData _$UserDataFromJson(Map<String, dynamic> json) => UserData(
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String,
+      email: json['email'] as String,
+      phone: json['phone'] as String,
+      token: json['token'] as String,
+    );
+
